b42b347f2a0af53202d9d8164a4b14697171afac
jakubsuran <suranjakub@gmail.com>
2021-12-05 19:45:25 +0100

    Added filter animation duration

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
   {{-- TODO: CSS fix, mobile filter --}}
-  <aside id="side-filter" class="transform md:transform-none -translate-x-full fixed md:sticky w-screen h-screen md:h-auto md:w-1/3 top-0 md:-top-10 md:block lg:w-1/4 py-10 float-left pr-8 bg-white bg-current z-50 md:overflow-auto transition-all">
+  <aside id="side-filter" class="transform md:transform-none -translate-x-full fixed md:sticky w-screen h-screen md:h-auto md:w-1/3 top-0 md:-top-10 md:block lg:w-1/4 py-10 float-left pr-8 bg-white bg-current z-50 md:overflow-auto transition-all duration-500">
     <svg id="close-side-filter" class="md:hidden w-10 h-10 ml-auto" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px;}</style></defs><title/><g id="cross"><line class="cls-1" x1="7" x2="25" y1="7" y2="25"/><line class="cls-1" x1="7" x2="25" y1="25" y2="7"/></g></svg>
     {{-- price filter --}}
     <div class="flex flex-col gap-2 mt-10">
